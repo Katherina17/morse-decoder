84623309fe8ad75d5def8795eb7018b9f9369de2
katherina17 <katherinepetrova76@gmail.com>
2022-02-22 17:44:03 +0300

    init: created a function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let space = '**********';
+    let result = "";
+    for (let i = 0; i < expr.length; i+=10){
+        let encodedLetter = expr.substr(i, 10);
+        if(encodedLetter == space) result += " ";
+        else {
+            let repr = "";
+            for(let j = 0; j < 10; j+=2){
+                if(encodedLetter.substr(j, 2) == "10"){
+                    repr +=".";
+                } else if(encodedLetter.substr(j, 2) == "11"){
+                    repr +="-";
+                }
+            }
+            result += MORSE_TABLE[repr];
+        }
+    }
+    return result;
+
 }
 
 module.exports = {
